5206c4444ca9625bf54c23ade0e8709fc7085814
abhishek25n <abhisheknomula@gmail.com>
2026-03-16 11:34:02 +0530

    done

---
+++ Task 3 spring/src/main/java/com/example/demo/runner.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class runner {
+
+    @Value("${app.name}")
+    private String appName;
+
+    @PostConstruct
+    public void printAppName() {
+        System.out.println("Application Name: " + appName);
+    }
+
+}
